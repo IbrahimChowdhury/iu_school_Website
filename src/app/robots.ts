f049f934d0a6ef71e9f5c6fe52c913352ad92f1f
Ibrahim Chowdhury <ibrahimchowdhury1000@gmail.com>
2023-11-05 20:22:01 +0600

    Robot.txt added

---
+++ src/app/robots.ts
@@ -0,0 +1,12 @@
+import { MetadataRoute } from 'next'
+ 
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: 'https://iu-school.vercel.app/',
+      disallow: 'https://iu-school.vercel.app/admin/',
+    },
+    sitemap: 'https://iu-school.vercel.app/sitemap.xml',
+  }
+}
