ec7198647dcd8d2e0bd953c80f65c70e92075d4e
Ibrahim Chowdhury <ibrahimchowdhury1000@gmail.com>
2023-11-05 20:12:02 +0600

    MetaDataRoute added in the sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,21 +1,23 @@
-export default async function sitemap(){
+import { MetadataRoute } from "next"
+
+export default  function sitemap(): MetadataRoute.Sitemap {
     const baseUrl="https://iu-school.vercel.app"
 
     return [
         {
-            url:baseUrl, lastModified:new Date()
+            url:baseUrl, lastModified:new Date(), priority:1
         },
         {
-            url:`${baseUrl}/notice`, lastModified:new Date()
+            url:`${baseUrl}/notice`, lastModified:new Date(), priority:0.8
         },
         {
-            url:`${baseUrl}/teachers`, lastModified:new Date()
+            url:`${baseUrl}/teachers`, lastModified:new Date(),priority:0.8
         },
         {
-            url:`${baseUrl}/students`, lastModified:new Date()
+            url:`${baseUrl}/students`, lastModified:new Date(), priority:0.8
         },
         {
-            url:`${baseUrl}/ClassRoutine`, lastModified:new Date()
+            url:`${baseUrl}/ClassRoutine`, lastModified:new Date(), priority:0.8
         },
 
     ]
