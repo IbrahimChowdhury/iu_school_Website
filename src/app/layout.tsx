bf38fc33282e4e02cdcb26f4793838f12580392b
Ibrahim Chowdhury <ibrahimchowdhury1000@gmail.com>
2023-11-05 22:35:49 +0600

    layout changes and metadata also changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,47 @@
-"use client"
 import Header from '@/components/header'
 import './globals.css'
-import { ThemeProvider } from 'next-themes'
 import Footer from '@/components/frontpage/footer'
 import { Metadata } from 'next'
+import { ThemeProvider } from "@/components/ThemeProvider";
 
 
-// export const metadata:Metadata  = {
-//   title: 'Islamic university',
-//   description: 'Islamic University Laboratory school and College',
-// }
+export const metadata:Metadata  = {
+  metadataBase: new URL("https://iu-school.vercel.app"),
+  title: "Iu School",
+  description: 'Islamic University Laboratory school and College',
+  applicationName:"iu School",
+  keywords: [
+    "IU School",
+    "Islamic University School",
+    "Vercel School Website",
+    "Education at IU School",
+    "Islamic University Laboratory School",
+    "IU School Programs",
+    "Academic Excellence",
+    "Admissions at IU School",
+    "Extracurricular Activities",
+    "Faculty and Staff",
+    "IU School Curriculum",
+    "Student Life",
+    "IU School Events",
+    "Scholarships and Financial Aid",
+    "Campus Facilities",
+    "Contact IU School",
+    "Parent-Teacher Communication",
+    "School News and Updates",
+    "Student Achievements",
+    "Alumni Network"
+  ],
+  authors: [{ name: 'ibrahim' }, { name: 'ibrahim Chowdhury', url: 'https://nextjs.org' }],
+  creator: 'Jiachi Liu',
+  publisher: 'Sebastian Markbåge',
+  alternates: {
+    canonical: '/',
+    languages: {
+      'en-US': '/en-US',
+    },
+  },
+}
 
 export default function RootLayout({
   children,
